d8bceb051ad868d7dffb8c0f16454f77a4266369
erlyg <erlyginegor@gmail.com>
2025-12-28 14:16:11 +0300

    изменил структуру кода и сделал фон

--- menu_class.py
+++ menu_class.py
@@ -1,27 +1,39 @@
 import arcade
+from arcade.gui import UITextureButton, UIManager, UIAnchorLayout, UIBoxLayout
 
 from pyglet.graphics import Batch
 
 
+# from main import GameView
+
 class MenuView(arcade.View):
     def __init__(self):
         super().__init__()
-        self.background_color = arcade.color.DARK_GREEN  # Фон для меню
+        self.background_color = arcade.color.DARK_GREEN
+
+        self.manager = UIManager()
+        self.manager.enable()
+
+        self.anchor_layout = UIAnchorLayout()
+
+        self.box_layout = UIBoxLayout(vertical=True, space_between=10)
+
+        # self.setup_widgets()
+
+        self.anchor_layout.add(self.box_layout, anchor_x="center", anchor_y="center")
+        self.manager.add(self.anchor_layout)
 
         self.batch = Batch()
-        self.main_text = arcade.Text("Главное Меню", self.window.width / 2, self.window.height / 2 + 50,
+        self.main_text = arcade.Text("Arcade Tank", self.window.width / 2, self.window.height / 2 + 100,
                                      arcade.color.WHITE, font_size=40, anchor_x="center", batch=self.batch)
-        self.space_text = arcade.Text("Нажми SPACE, чтобы начать!", self.window.width / 2, self.window.height / 2 - 50,
-                                      arcade.color.WHITE, font_size=20, anchor_x="center", batch=self.batch)
+        # self.space_text = arcade.Text("Нажми SPACE, чтобы начать!", self.window.width / 2, self.window.height / 2 - 150,
+        #                             arcade.color.WHITE, font_size=20, anchor_x="center", batch=self.batch)
 
     def on_draw(self):
         self.clear()
         self.batch.draw()
+        # self.manager.draw()
 
-    def on_key_press(self, key, modifiers):
-        if key == arcade.key.SPACE:
-            game_view = GameView()  # Создаём игровой вид
-            self.window.show_view(game_view)  # Переключаем
 
 window = arcade.Window(800, 600, "")
 menu_view = MenuView()
